0e659586f6e2a00fc6f553a4a86a3a41bcde3b9a
hchao <helenchao01@gmail.com>
2024-07-22 16:01:29 -0700

    Add new file

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello, World!")
-
 function getComputerChoice() {
   num = Math.random();
   if (num >= 0 && num < 0.33) {
@@ -21,45 +19,20 @@ function getHumanChoice() {
   return humanChoice;
 }
 
-// function playRound(humanChoice, computerChoice) {
-//   humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1);
-//   computerChoice = computerChoice [0].toUpperCase() + computerChoice.slice(1);
-  
-//   let winner = "Tie";
-//   if ((humanChoice == "Rock" && computerChoice == "Scissors") || 
-//   (humanChoice == "Scissors" && computerChoice == "Paper") ||
-//   (humanChoice == "Paper" && computerChoice == "Rock")) {
-//     winner = "Human";
-//   }
-//   else if ((computerChoice == "Rock" && humanChoice == "Scissors") || 
-//   (computerChoice == "Scissors" && humanChoice == "Paper") ||
-//   (computerChoice == "Paper" && humanChoice == "Rock")) {
-//     winner = "Computer";
-//   }
-
-//   if (winner == "Human") {
-//     humanScore = humanScore + 1
-//     console.log(`You won. ${humanChoice} beats ${computerChoice}.`)
-//   }
-//   else if (winner == "Computer") {
-//     computerScore = computerScore + 1
-//     console.log(`You lost. ${computerChoice} beats ${humanChoice}.`)
-//   }
-//   else {
-//     console.log(`You tied. Both of you played ${computerChoice}.`)
-//   }
-// }
-
 function playGame() {
 
+  // Variables are accessible by playRound, so long as playRound func is declared inside playGame
   let humanScore = 0;
   let computerScore = 0;
 
   function playRound(humanChoice, computerChoice) {
+    // Reformats choice by capitalizing the first letter
     humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1);
     computerChoice = computerChoice [0].toUpperCase() + computerChoice.slice(1);
     
+    // Default outcome of round is "Tie"
     let winner = "Tie";
+    
     if ((humanChoice == "Rock" && computerChoice == "Scissors") || 
     (humanChoice == "Scissors" && computerChoice == "Paper") ||
     (humanChoice == "Paper" && computerChoice == "Rock")) {
@@ -71,6 +44,7 @@ function playGame() {
       winner = "Computer";
     }
   
+    // Update scores based on winner
     if (winner == "Human") {
       humanScore = humanScore + 1
       console.log(`You won. ${humanChoice} beats ${computerChoice}.`)
@@ -84,12 +58,15 @@ function playGame() {
     }
   }
 
-  for (let i = 0; i < 5; i++) {
+  // Play five rounds by iterating from 0 to 4
+  const numberOfRounds = 5;
+  for (let i = 0; i < numberOfRounds; i++) {
     let humanSelection = getHumanChoice();
     let computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection);
   }
 
+  // Determine overall winner
   if (humanScore > computerScore) {
     console.log(`You won ${humanScore} games out of 5!`)
   }
